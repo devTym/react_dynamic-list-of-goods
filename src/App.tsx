0304d0dcc4a778070f83c1132d818f2f12a3a6dc
DevT <dev.tymofiichuk@gmail.com>
2026-02-11 15:30:29 +0200

    Dynamic list of goods task

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './App.scss';
 import { GoodsList } from './GoodsList';
 
 // import { getAll, get5First, getRed } from './api/goods';
 // or
-// import * as goodsAPI from './api/goods';
+import * as goodsAPI from './api/goods';
+import { Good } from './types/Good';
 
-export const App: React.FC = () => (
-  <div className="App">
-    <h1>Dynamic list of Goods</h1>
+export const App: React.FC = () => {
+  const [goods, setGoods] = useState<Good[]>([]);
 
-    <button type="button" data-cy="all-button">
-      Load all goods
-    </button>
+  const loadAllGoodsHandler = () => {
+    goodsAPI
+      .getAll()
+      .then(data => setGoods(data))
+      .catch(error => {
+        // eslint-disable-next-line no-console
+        console.error('Failed to load goods:', error);
+      });
+  };
 
-    <button type="button" data-cy="first-five-button">
-      Load 5 first goods
-    </button>
+  const load5FirstGoodsHandler = () => {
+    goodsAPI
+      .get5First()
+      .then(data => setGoods(data))
+      .catch(error => {
+        // eslint-disable-next-line no-console
+        console.error('Failed to load First 5 goods:', error);
+      });
+  };
 
-    <button type="button" data-cy="red-button">
-      Load red goods
-    </button>
+  const loadRedGoodsHandler = () => {
+    goodsAPI
+      .getRedGoods()
+      .then(data => setGoods(data))
+      .catch(error => {
+        // eslint-disable-next-line no-console
+        console.error('Failed to load Red goods:', error);
+      });
+  };
 
-    <GoodsList goods={[]} />
-  </div>
-);
+  return (
+    <div className="App">
+      <h1>Dynamic list of Goods</h1>
+
+      <button type="button" data-cy="all-button" onClick={loadAllGoodsHandler}>
+        Load all goods
+      </button>
+
+      <button
+        type="button"
+        data-cy="first-five-button"
+        onClick={load5FirstGoodsHandler}
+      >
+        Load 5 first goods
+      </button>
+
+      <button type="button" data-cy="red-button" onClick={loadRedGoodsHandler}>
+        Load red goods
+      </button>
+
+      <GoodsList goods={goods} />
+    </div>
+  );
+};
